eadbc2164c95f3810bcc40c81c41c8667aef5a83
jk872001 <jiteshkumar872001@gmail.com>
2022-11-10 10:31:43 +0530

    selection sort algorithm

--- lec8/selectionSort.js
+++ lec8/selectionSort.js
@@ -1,17 +1,17 @@
-const bubbleSort = (arr) => {
+const selectionSort = (arr) => {
     let n = arr.length;
     for (let i = 0; i < n; i++) {
-      for (let j = 0; j < n - i - 1; j++) {
-        if (arr[j] > arr[j + 1]) {
-          let temp = arr[j];
-          arr[j] = arr[j + 1];
-          arr[j + 1] = temp;
+        minIndex=i;
+      for (let j = i+1; j < n; j++) {
+        if (arr[j] < arr[minIndex]) {
+            minIndex=j;
         }
       }
+      [arr[i],arr[minIndex]]=[arr[minIndex],arr[i]]
     }
     return arr;
   };
   
   const arr = [1, 5, 64, 35, 7, 43, 22];
-  console.log(bubbleSort(arr));
+  console.log(selectionSort(arr));
   
