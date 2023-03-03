848b9f7ce96fd8b69dbfbb615864cac006488b8f
jk872001 <jiteshkumar872001@gmail.com>
2023-03-03 12:24:42 +0530

    lecture 4 done

--- codeStepByStep/index.js
+++ codeStepByStep/index.js
@@ -1,24 +1,22 @@
-let arr=[2,4,12,3,43,6,56,7,46,32,92,10]
+let arr = [2, 4, 12, 3, 43, 6, 56, 7, 46, 32, 92, 10]
 
-function funcExecute()
-{
-    let inputValue=document.getElementById("input").value
-    if(inputValue.length>0)
-    {
+function funcExecute() {
+    let inputValue = document.getElementById("input").value
+    if (inputValue.length > 0) {
         alert(`Arr value is ${arr[inputValue]}`)
-       
+
     }
-    else{
+    else {
         alert("Please Enter valid number")
     }
-   
+
 }
 
 
 // insert an element in the arr
-let arr2=[2,4,12,92,16]
-let newEle=10
-let position=2
+let arr2 = [2, 4, 12, 92, 16]
+let newEle = 10
+let position = 2
 
 // logically
 
@@ -39,3 +37,13 @@ let position=2
 // let value=arr2.splice(2,0,"10");
 // console.log(value);
 
+// delete an element from the arr
+let arr3 = [2, 4, 42, 23, 16]
+// delete the element at index 
+let ind = 4;
+
+for (let i = ind; i < arr3.length - 1; i++) {
+    arr3[i] = arr3[i + 1];
+}
+arr3.pop()
+console.log(arr3);
